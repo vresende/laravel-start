b1c2ffa74aecbfb5192f289b53ce70882623d429
vresende <vini_handball@hotmail.com>
2024-01-02 16:54:59 -0300

    criar-componentes-de-formulario-5: Added choise.js, and optimizations made to maintain the layout according to Bootstrap.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import '~bootstrap';
+import Choices from "choices.js";
+
+//Carrega o select com as opções
+const choices = new Choices('.js-choice',{
+    loadingText: 'Carregando...',
+    noResultsText: 'Nenhum resultado encontrado',
+    noChoicesText: 'Não há opções para escolher',
+    itemSelectText: 'aperte "Enter" para selecionar',
+    uniqueItemText: 'Apenas valores únicos podem ser adicionados',
+    customAddItemText: 'Apenas valores que correspondem a condições específicas podem ser adicionados',
+
+});
+
+
+
+
