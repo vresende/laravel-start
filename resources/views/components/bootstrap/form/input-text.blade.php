9a850aa2a612f2fddac5b65d7e3f81c888c37ed1
vresende <vini_handball@hotmail.com>
2024-01-02 14:17:53 -0300

    criar-componentes-de-formulario-5: Adjustment and correction in the input-text component.

--- resources/views/components/bootstrap/form/input-text.blade.php
+++ resources/views/components/bootstrap/form/input-text.blade.php
@@ -1,12 +1,14 @@
-@props(['id', 'name', 'label', 'grid', 'required' => false])
+@props(['id', 'name', 'label'])
 
-<div class="{{ $grid }}">
-    <label for="{{ $id }}" class="form-label">{{ $label }}</label>
-    <input type="text" name="{{ $name }}" id="{{ $id }}"
+<div class="form-group">
+    <label for="{{ $id }}" class="form-label">{{ __($label) }}</label> <!-- Tradução para o label -->
+    <input
+        type="text"
+        id="{{ $id }}"
+        name="{{ $name }}"
         {{ $attributes->merge(['class' => 'form-control'])->class([$errors->has($name) ? 'is-invalid' : '']) }}
-        {{ $required ? 'required' : '' }}>
-
+    >
     @error($name)
-    <div class="invalid-feedback">{{ $message }}</div>
+    <div class="invalid-feedback">{{ __($message) }}</div> <!-- Tradução para a mensagem de erro -->
     @enderror
 </div>
--- tests/Feature/View/Components/bootstrapInputTextTest.php
+++ tests/Feature/View/Components/bootstrapInputTextTest.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ViewErrorBag;
-use Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
 
 beforeEach(function () {
     // Simula a variável global $errors
@@ -16,17 +13,16 @@ it('renders the text input component', closure: function () {
     $view = $this->blade('<x-bootstrap.form.input-text
         name="test"
         id="test-id"
-        grid="col-md-6"
         label="Test Label"
         value="Test Value"
         placeholder="Test Placeholder"
         required="true"
-        />', ['required' => true]);
+        />');
 
     $html = (string)$view; // Converte o resultado da view para string
 
     expect($html)->not->toBeEmpty()
-        ->and($html)->toContain('id="test-id"', 'class="col-md-6"', 'for="test-id"', 'value="Test Value"', )
+        ->and($html)->toContain('id="test-id"', 'for="test-id"', 'value="Test Value"', )
         ->and($html)->toContain('required', 'Test Label', 'Test Placeholder');
 
 });
